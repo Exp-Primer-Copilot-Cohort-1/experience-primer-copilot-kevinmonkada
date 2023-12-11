d942816fd93e2b482c9898d7f00affc1798ac568
Kevin Moncada <104812734+kevinmonkada@users.noreply.github.com>
2023-12-11 03:51:16 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,22 @@
+//create a web server
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const { Comment } = require('../models');
+
+//GET /comments
+router.get('/', (req, res) => {
+  db.Comment.findAll()
+    .then(comments => {
+      res.json(comments);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+//POST /comments
+router.post('/', (req, res) => {
+  db.Comment.create({
+    text: req.body.text,
